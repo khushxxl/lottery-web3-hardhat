e4aa8571348da92980b337c8d3b03e78151c77e3
khushxxl <khushaal.choithramani@gmail.com>
2022-01-07 11:05:46 +0530

    lottery v2

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,11 +4,8 @@ module.exports = {
   solidity: '0.4.17',
   networks: {
     ropsten: {
-      url:
-        'https://eth-ropsten.alchemyapi.io/v2/84QznWTEamOBgmDEEUnqZhZ4SUhZQF73',
-      accounts: [
-        '5957a3cd3e4c8f220ed43435153eab787d35c1bb10263932d515241d5baa5ccd',
-      ],
+      url: process.env.ROPSTEN_URL,
+      accounts: [process.env.BLOCKCHAIN_CODE],
     },
   },
 }
