fb48cf3461c60af5a7ea4aefc01c228a313f64ec
khushxxl <khushaal.choithramani@gmail.com>
2022-01-07 16:35:20 +0530

    website v6

--- utils/constants.js
+++ utils/constants.js
@@ -1,4 +1,6 @@
-import { abi } from '../artifacts/contracts/Lottery.sol/Lottery.json'
+// import   { abi } from '../artifacts/contracts/Lottery.sol/Lottery.json'
+const abi = require('../artifacts/contracts/Lottery.sol/Lottery.json')
+
 export const contractAddress = '0x585d94fB0445BaeB6c7A8eC4E89960A6E27321CD'
 
 export const contractABI = abi.abi
